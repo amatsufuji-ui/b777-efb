e5f526d3c628c03008c961841aece16a7c954fd3
fly4u88 <fly4u88@gmail.com>
2026-08-23 18:40:08 +0900

    NOw8.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import * as LucideIcons from 'lucide-react';
 // =========================================================================
 // ★ アプリバージョン設定
 // =========================================================================
-const APP_VERSION = "8.0";
+const APP_VERSION = "8.1";
 // =========================================================================
 
 import { RAW_CSV_DATA, aircraftRegistrationList, BUDDYCOM_LINKS } from './data/flightData';
